b98252fbbaa09b8428868030bf857e0058b157d6
edumo <contact@edumo.net>
2014-08-10 14:27:40 +0200

    glsl compiler error on Mac #1
    using literal 1.0 instead of 1

--- Dof/data/dof/colorfrag.glsl
+++ Dof/data/dof/colorfrag.glsl
@@ -8,5 +8,5 @@ uniform float maxDepth;
 
 void main() {
   float depth = gl_FragCoord.z / gl_FragCoord.w;
-  gl_FragColor = vec4(vec3(1 - depth/maxDepth), 1);
+  gl_FragColor = vec4(vec3(1.0 - depth/maxDepth), 1.0);
 }
